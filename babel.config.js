313fbb992550b10693c7e7f8b16ec9ea4237c845
whai2 <whai2125@gmail.com>
2024-08-07 13:35:27 +0900

    build: ios 앱 빌드 준비

--- babel.config.js
+++ babel.config.js
@@ -15,3 +15,10 @@ module.exports = function (api) {
     ]
   };
 };
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo']
+//   };
+// };
